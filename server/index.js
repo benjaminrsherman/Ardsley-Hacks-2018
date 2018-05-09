d9d2c56f13014d0cb4e199090bc0f111226ecbc3
Benjamin Sherman <15524035+benjaminrsherman@users.noreply.github.com>
2018-05-09 15:09:34 -0400

    Map files can now be uploaded

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.post('/score', (req, res) => {
   var score = req.body.score
   var str = "touch 'data/" + name + '@' + map_id + '@' + score + "'"
   exec(str)
-  console.log("Executing command: " + str)
+  console.log("Uploading " + name + "'s score of " + score + " on " + map_id)
 })
 
 app.get('/score', (req, res) => {
@@ -23,6 +23,13 @@ app.get('/score', (req, res) => {
   var score = ""
   exec("ls -1 /data | grep '@" + map_id + "@' | sort -t '@" + map_id + "@' -k 2 | head -1 | cut -f 3 -d '@'", // unix piping amirite
     (err, stdout, stderr) => { res.send(stdout) } )
+  console.log("Retrieving score for map " + map_id)
 })
+
+app.post('/map', (req, res) => {
+  var map_id = req.body.map_id
+  exec("echo '" + req.body + "' >> data/" + map_id + ".AHMAP")
+  console.log("Uploaded map " + map_id)
+}
 
 app.listen(3000, () => console.log("Listening on port 3000"))
