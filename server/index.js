f1710588253b3ea160708ba7b4fb90846316e738
Benjamin Sherman <15524035+benjaminrsherman@users.noreply.github.com>
2018-05-09 13:26:15 -0400

    Forgive me for this concurrency solution

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
+var exec = require('child_process').exec
+
 var express = require('express')
 var app = express()
 
-app.get('/', function(req, res) {
+app.use(express.json())
+
+app.get('/', (req, res) => {
   res.send('pls no hack thx')
+})
+
+app.post('/score', (req, res) => {
+  var name = req.body.name
+  var score = req.body.score
+  var str = 'touch data/' + name + '@' + score
+  exec(str)
 })
 
 app.listen(3000, () => console.log("Listening on port 3000"))
