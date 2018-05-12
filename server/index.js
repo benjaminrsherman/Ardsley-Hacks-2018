a0cb3d57d85b51c065feff0a33ba8f758e9e106c
Benjamin Sherman <15524035+benjaminrsherman@users.noreply.github.com>
2018-05-12 09:44:26 -0400

    Server now only uses GET requests

--- server/index.js
+++ server/index.js
@@ -13,38 +13,39 @@ app.use(function(req, res, next) {
 
 app.get('/', (req, res) => {
   res.send('pls no hack thx')
+  console.log(req.query)
 })
 
-app.post('/score', (req, res) => {
-  var name = req.body.name
-  var map_id = req.body.map_id
-  var score = req.body.score
+app.get('/post-score', (req, res) => {
+  var name = req.query.name
+  var map_id = req.query.map_id
+  var score = req.query.score
   var str = "touch 'data/" + name + '@' + map_id + '@' + score + "'"
   exec(str)
   console.log("Uploading " + name + "'s score of " + score + " on " + map_id)
 })
 
-app.get('/score', (req, res) => {
-  var map_id = req.body.map_id
+app.get('/get-score', (req, res) => {
+  var map_id = req.query.map_id
   var score = ""
   exec("ls -1 ./data | grep '@" + map_id + "@' | sort -t '@" + map_id + "@' -k 2 | head -1 | cut -f 3 -d '@'", // unix piping amirite
     (err, stdout, stderr) => { res.send(stdout) } )
   console.log("Retrieving score for map " + map_id)
 })
 
-app.get('/top-score', (req, res) => {
+app.get('/get-top-score', (req, res) => {
   exec("ls -1 ./data | awk -F '@' '{print $3}' | sort | sed '1!G;h;$!d' | head -1",
     (err, stdout, stderr) => { res.send(stdout) });
   console.log("Retrieving top score")
 })
 
-app.post('/map', (req, res) => {
+app.get('/post-map', (req, res) => {
   var map_id = req.body.map_id
   exec("echo '" + req.body + "' >> data/" + map_id + ".AHMAP")
   console.log("Uploaded map " + map_id)
 })
 
-app.get('/map', (req, res) => {
+app.get('/get-map', (req, res) => {
   var map_id = req.body.map_id
   var str = ""
   exec("cat data/" + map_id + ".AHMAP",
@@ -53,10 +54,13 @@ app.get('/map', (req, res) => {
   console.log("Retrieving map " + map_id)
 })
 
-app.get('/total-maps', (req, res) => {
+app.get('/get-total-maps', (req, res) => {
   exec("ls -1 ./data | awk -F '@' '{print $2}' | uniq | wc -l",
     (err, stdout, stderr) => { res.send(stdout) })
   console.log("Retrieving total number of maps")
 })
+
+app.get('/online', (req, res) => {
+
 
 app.listen(3000, () => console.log("Listening on port 3000"))
